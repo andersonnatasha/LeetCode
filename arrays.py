a52e78494425c4664ec00adfe31317a102518298
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-20 22:21:01 +0000

    LeetCode Problem #1 twoSum

--- arrays.py
+++ arrays.py
@@ -86,6 +86,38 @@ def findDisappearedNumbers(nums):
     return list(range_set)  # o(n)
 
 
+def twoSum(nums, target):
+    """Given an array of integers nums and an integer target,
+    return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution,
+    and you may not use the same element twice.
+
+    You can return the answer in any order.
+    >>> twoSum([2,7,11,15],9)
+    [0, 1]
+    >>> twoSum([3,2,4],6)
+    [1, 2]
+    >>> twoSum([3,3],6)
+    [0, 1]
+    >>> twoSum([3,3,1],7)
+    'No matches'
+
+    """
+
+    nums_as_dict = {}
+
+    for index, num in enumerate(nums):
+        difference = target - num
+        if difference in nums_as_dict.values() and index != nums.index(difference):
+            return [nums.index(difference), index]
+        nums_as_dict[index] = num
+
+    return "No matches"
+
+    # runtime of O(n)
+
+
 if __name__ == '__main__':
     import doctest
 
