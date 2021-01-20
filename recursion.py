5f2aff80cce7646ac7af62730d6d496f0939cc9e
Natasha Anderson <andersonnatashaa@gmail.com>
2021-01-20 01:14:58 +0000

    LeetCode Problem 1. Two Sum

--- recursion.py
+++ recursion.py
@@ -105,6 +105,41 @@ def removeDuplicates(nums):
     return find_duplicates(nums, 0)
 
 
+def twoSum(nums, target):
+    """Given an array of integers nums and an integer target,
+    return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+    >>> twoSum([2,7,11,15],9)
+    [0, 1]
+    >>> twoSum([3,2,4],6)
+    [1, 2]
+    >>> twoSum([3,3],6)
+    [0, 1]
+    """
+
+    if len(nums) == 2:
+        return [0, 1]
+
+    def find_sum(nums, target, i):
+
+        j = 0
+        while j < len(nums):
+            if i == j:
+                j += 1
+                continue
+            else:
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                else:
+                    j += 1
+        return find_sum(nums, target, i+1)
+
+    return find_sum(nums, target, 0)
+
+
 if __name__ == "__main__":
     import doctest
 
