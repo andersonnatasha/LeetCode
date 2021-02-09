395dc413d722bfcd5a7246bce6f04340e5ee3c78
Natasha Anderson <natasha@Natashas-MacBook-Air.local>
2021-02-08 17:16:43 -0800

    392. Is Subsequence

--- strings.py
+++ strings.py
@@ -126,6 +126,35 @@ def reverseString(s):
 
     return s
 
+def isSubsequence(s,t):
+    """Given two strings s and t, check if s is a subsequence of t.
+    A subsequence of a string is a new string that is formed from the original string by deleting some
+    (can be none) of the characters without disturbing the relative positions of the remaining characters.
+    (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+    >>> isSubsequence("abc", "ahbgdc")
+    True
+
+    >>> isSubsequence("abx", "ahbgdc")
+    False
+    >>> isSubsequence("aa", "ba")
+    False
+    """
+
+    s_pointer = 0
+    t_pointer = 0
+
+    while s_pointer < len(s) and t_pointer < len(t):
+        if s[s_pointer] == t[t_pointer]:
+            s_pointer += 1
+            t_pointer += 1
+        else:
+            t_pointer +=1
+
+    return s_pointer == len(s)
+
+
+
 
 if __name__ == "__main__":
     import doctest
