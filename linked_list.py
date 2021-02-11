a909a99419bc5ff2016fcd321d04cbe9bdfdfc80
Natasha Anderson <natasha@Natashas-MacBook-Air.local>
2021-02-11 10:54:17 -0800

    Problem 141. Linked List Cycle

--- linked_list.py
+++ linked_list.py
@@ -36,6 +36,26 @@ def mergeTwoLists(l1, l2):
 
     return merged_list.next
 
+def hasCycle(head):
+    """Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+    Return true if there is a cycle in the linked list. Otherwise, return false."""
+
+    if not head:
+        return False
+
+    fast_node = head.next
+    slow_node = head
+
+    while fast_node != None and fast_node.next != None:
+        if fast_node == slow_node:
+            return  True
+        fast_node = fast_node.next.next
+        slow_node = slow_node.next
+    return False
+
 
 if __name__ == "__main__":
     import doctest
